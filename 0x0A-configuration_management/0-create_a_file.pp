614465448bac4b40b03465d903f65b3d871cbd87
Muna Saeed <munasaeed6367@gmail.com>
2024-01-19 15:08:55 -0800

    done

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# 0-create_a_file.pp - Puppet Manifest to create a file in /tmp
+
+file { '/tmp/school':
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
