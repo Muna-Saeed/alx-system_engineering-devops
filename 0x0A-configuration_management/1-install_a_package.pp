2ed9dc4e988bacdd887bd6352cb2813050724520
Muna Saeed <munasaeed6367@gmail.com>
2024-01-24 06:31:59 -0800

    updated

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,19 +1,12 @@
 # 1-install_a_package.pp - Puppet Manifest to install Flask package
 
-package { 'python3-pip':
-  ensure => 'installed',
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
 }
 
-package { 'libffi-dev':
-  ensure => 'installed',
-}
-
-package { 'libssl-dev':
-  ensure => 'installed',
-}
-
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  path    => ['/usr/bin', '/usr/local/bin'],
-  creates => '/usr/local/lib/python3.8/dist-packages/flask',
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['werkzeug'],
 }
