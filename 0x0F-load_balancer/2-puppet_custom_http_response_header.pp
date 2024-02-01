32150ebc80d63caee4f9ef10c3a9323edf8311e1
Muna Saeed <munasaeed6367@gmail.com>
2024-02-01 12:38:08 -0800

    done

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,52 @@
+# File: 7-puppet_install_nginx_web_server.pp
+
+# Install Nginx package
+package { 'nginx':
+  ensure => 'installed',
+}
+
+# Configure Nginx server
+file { '/var/www/html/index.html':
+  ensure  => 'present',
+  content => 'Hello World!',
+}
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => 'present',
+  content => '
+server {
+    listen 80;
+    server_name _;
+
+    location / {
+        root   /var/www/html;
+        index  index.html;
+    }
+
+    location /redirect_me {
+        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+    }
+
+    error_page 404 /404.html;
+    location = /404.html {
+        root /usr/share/nginx/html;
+        internal;
+    }
+	
+   add_header X-Served-By \$hostname;
+}
+',
+}
+
+# Enable the site by creating a symbolic link
+file { '/etc/nginx/sites-enabled/default':
+  ensure => 'link',
+  target => '/etc/nginx/sites-available/default',
+}
+
+# Restart Nginx service
+service { 'nginx':
+  ensure    => 'running',
+  enable    => true,
+  subscribe => [File['/etc/nginx/sites-available/default'], Package['nginx']],
+}
