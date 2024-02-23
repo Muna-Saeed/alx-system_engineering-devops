65d5beecea009a77a2b1a1a22c869ef39f63d524
Muna Saeed <munasaeed6367@gmail.com>
2024-02-23 16:12:29 +0300

    updated

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,10 +5,10 @@ Description: Python script that, using a REST API, returns
 information about an employee's TODO list progress.
 """
 
-import json
-import os
+
 import requests
 import sys
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2 or not sys.argv[1].isdigit():
@@ -21,7 +21,6 @@ if __name__ == "__main__":
     base_url = "https://jsonplaceholder.typicode.com"
     user_url = f"{base_url}/users/{employee_id}"
     todos_url = f"{base_url}/todos?userId={employee_id}"
-
     try:
         user_response = requests.get(user_url)
         todos_response = requests.get(todos_url)
@@ -47,21 +46,3 @@ if __name__ == "__main__":
     for todo in todos:
         if todo["completed"]:
             print(f"\t{todo['title']}")
-
-    # Writing to individual user file
-    task_list = [{"username": user['name'], "task": todo['title'], "completed": todo['completed']} for todo in todos]
-    filename = f"{employee_id}.json"
-    with open(filename, "w") as json_file:
-        json.dump({employee_id: task_list}, json_file)
-
-    # Writing to overall file
-    all_tasks_filename = "todo_all_employees.json"
-    all_tasks_data = {}
-    if os.path.exists(all_tasks_filename):
-        with open(all_tasks_filename, "r") as all_tasks_file:
-            all_tasks_data = json.load(all_tasks_file)
-
-    all_tasks_data[employee_id] = task_list
-
-    with open(all_tasks_filename, "w") as all_tasks_file:
-        json.dump(all_tasks_data, all_tasks_file, indent=4)
