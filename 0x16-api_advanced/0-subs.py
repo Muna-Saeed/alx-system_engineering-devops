14693df072bf57235f03ce543c4cec9451a8ee8d
Muna Saeed <munasaeed6367@gmail.com>
2024-03-07 15:24:20 -0800

    mandatory tasks done

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+0-subs
+"""
+import requests
+
+def number_of_subscribers(subreddit):
+    """
+    Returns the number of subscribers for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        int: The number of subscribers, or 0 if the subreddit is invalid.
+    """
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            return data['data']['subscribers']
+        else:
+            return 0
+    except Exception as e:
+        print(f"Error: {e}")
+        return 0
+
+if __name__ == '__main__':
+    import sys
+
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        print("{:d}".format(number_of_subscribers(sys.argv[1])))
---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+1-top_ten
+"""
+import requests
+
+def top_ten(subreddit):
+    """
+    Prints the titles of the first 10 hot posts for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        None
+    """
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()['data']['children']
+
+            for post in data[:10]:
+                print(post['data']['title'])
+        else:
+            print(None)
+    except Exception as e:
+        print(f"Error: {e}")
+        print(None)
+
+if __name__ == '__main__':
+    import sys
+
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        top_ten(sys.argv[1])
