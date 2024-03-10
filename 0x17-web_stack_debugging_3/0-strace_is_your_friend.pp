c9a07de67273321586860123559cc7737fef24e1
Muna Saeed <munasaeed6367@gmail.com>
2024-03-10 13:45:27 -0700

    done

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -9,10 +9,3 @@ exec { 'fix-apache':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => '/usr/local/bin/:/bin/'
 }
-
-# Ensure Apache is running and restart if necessary
-service { 'apache2':
-  ensure  => running,
-  enable  => true,
-  require => File['/etc/apache2/sites-available/000-default.conf'],
-}
