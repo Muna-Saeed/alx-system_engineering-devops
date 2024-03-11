ec82160668ed34279b4712f7d0865eee8d4f90cf
Muna Saeed <munasaeed6367@gmail.com>
2024-03-11 13:36:54 -0700

    done

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,24 +1,43 @@
 #!/usr/bin/python3
-'''
-    this module contains the function top_ten
-'''
+"""
+1-top_ten
+"""
 import requests
-from sys import argv
 
 
 def top_ten(subreddit):
-    '''
-        returns the top ten posts for a given subreddit
-    '''
-    user = {'User-Agent': 'Lizzie'}
-    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
-                       .format(subreddit), headers=user).json()
+    """
+    Prints the titles of the first 10 hot posts for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        None
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {
+            "User-Agent": "Mozilla/5.0 Chrome/58.0.3029.110 Safari/537.3"
+            }
+
     try:
-        for post in url.get('data').get('children'):
-            print(post.get('data').get('title'))
-    except Exception:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()["data"]["children"]
+
+            for post in data[:10]:
+                print(post["data"]["title"])
+        else:
+            print(None)
+    except Exception as e:
+        print(f"Error: {e}")
         print(None)
 
 
 if __name__ == "__main__":
-    top_ten(argv[1])
+    import sys
+
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        top_ten(sys.argv[1])
