c4acabfd6abb74a05b33f43c73f1c5bd3175c41b
Muna Saeed <munasaeed6367@gmail.com>
2024-03-11 13:01:48 -0700

    done~

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-""" Module for task 0 """
-import requests
+"""Module for task 0"""
 
 
 def number_of_subscribers(subreddit):
-    """ Queries the Reddit API of the users"""
-    req = requests.get(
-        "https://www.reddit.com/r/{}/about.json".format(subreddit),
-        headers={"User-Agent": "Custom"},
-    )
+    """Queries the Reddit API and returns the number of subscribers
+    to the subreddit"""
+    import requests
 
-    if req.status_code == 200:
-        return req.json().get("data").get("subscribers", 0)
-    else:
+    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
         return 0
+
+    return sub_info.json().get("data").get("subscribers")
